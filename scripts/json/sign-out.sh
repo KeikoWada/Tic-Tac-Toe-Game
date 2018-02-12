212a1c8d153c4e459a6d01813f072b6c14c8db06
KeikoWada <kwaddy71385@gmail.com>
2018-02-12 18:44:41 -0500

    fixed curl request for sign out
    fixed the curl request for sign out

--- scripts/json/sign-out.sh
+++ scripts/json/sign-out.sh
@@ -2,10 +2,9 @@
 
 # ID=2 sh scripts/sign-out.sh
 
-curl "https://aqueous-atoll-85096.herokuapp.com/sign-out/:id{$ID}" \
+curl "http://tic-tac-toe.wdibos.com/sign-out/{$ID}" \
   --include \
   --request DELETE \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
-
 echo
