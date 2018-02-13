df5a481c8e2e66a0ad65a1f8cc3849215e807b40
KeikoWada <kwaddy71385@gmail.com>
2018-02-13 17:27:53 -0500

    no changes on this
    no changes on this

--- assets/scripts/game/api.js
+++ assets/scripts/game/api.js
@@ -36,7 +36,29 @@ const updateGame = function (index, value, over) {
   })
 }
 
+const tracker = function (index, value, over) {
+  console.log(store, value, over)
+  return $.ajax({
+    url: config.apiOrigin + '/games/' + store.game,
+    method: 'PATCH',
+    headers: {
+      contentType: 'application/json',
+      Authorization: 'Token token=' + store.user.token
+    },
+    data: {
+      'game': {
+        'cell': {
+          'index': index,
+          'value': value
+        },
+        'over': over
+      }
+    }
+  })
+}
+
 module.exports = {
   create,
-  updateGame
+  updateGame,
+  tracker
 }
